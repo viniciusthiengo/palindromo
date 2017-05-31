58aa1d2a08d6ab59b578f6cf280e7334cfe37a09
Vinícius Thiengo <viniciusthiengo@gmail.com>
2017-05-30 22:38:36 -0300

    Initial commit

---
+++ app/src/main/java/br/com/thiengo/palindromo/MainActivity.java
@@ -0,0 +1,39 @@
+package br.com.thiengo.palindromo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btVerificar = (Button) findViewById(R.id.bt_verificar);
+        btVerificar.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText etPalindromo = (EditText) findViewById(R.id.et_palindromo);
+        TextView tvResposta = (TextView)  findViewById(R.id.tv_resposta);
+        Palindromo palindromo = new Palindromo( etPalindromo.getText().toString() );
+        String resposta;
+
+        if( palindromo.ehPalindromo() ){
+            resposta = " é um palíndromo";
+        }
+        else{
+            resposta = " NÃO é um palíndromo";
+        }
+        resposta = palindromo.getConteudo() + resposta;
+
+        tvResposta.setText( resposta );
+    }
+}
---
+++ app/src/main/java/br/com/thiengo/palindromo/Palindromo.java
@@ -0,0 +1,20 @@
+package br.com.thiengo.palindromo;
+
+
+public class Palindromo {
+    private String conteudo;
+
+    public Palindromo( String c ){
+        this.conteudo = c;
+    }
+
+    public boolean ehPalindromo(){
+        String invertido = new StringBuilder( conteudo ).reverse().toString();
+
+        return invertido.equalsIgnoreCase(conteudo);
+    }
+
+    public String getConteudo(){
+        return conteudo.toLowerCase();
+    }
+}
